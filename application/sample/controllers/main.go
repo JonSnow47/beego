7d6a300f887268685e9bedce4ca2934a4632e077
JonSnow47 <745539141@qq.com>
2017-12-27 19:19:17 +0800

    sample

---
+++ application/sample/controllers/main.go
@@ -0,0 +1,26 @@
+package controllers
+
+import (
+	"github.com/astaxie/beego"
+	"fmt"
+)
+type MainController struct {
+    beego.Controller
+}
+
+func (this *MainController) Get() {
+	u := this.Ctx.Request.FormValue("user")
+	fmt.Println("hjkl:",u)
+    this.Ctx.ResponseWriter.Write([]byte(`
+         <html>
+           <head>
+             <title>Chat</title>
+           </head>
+           <body>
+             Let's chat!
+           </body>
+</html>`))
+
+
+    this.ServeJSON()
+}
